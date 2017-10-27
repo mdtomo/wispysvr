ce637ee3bf4b0184e5064d02073a3582b1ece1bd
Matthew Thomas <mdthomas@hotmail.ca>
2017-10-26 20:29:27 -0700

    JWT now using cookies instead of headers. Added socket io.

--- static/js/login.js
+++ static/js/login.js
@@ -1,72 +1,46 @@
-// Wispysvr Login handler
-$(document).ready(function() {
-    console.log('ready');
-    if (window.localStorage.token) {
-        window.localStorage.removeItem('token');
-        updateStatus('Logged out successfully.', 'success');
+// Wispysvr login
+var app = new Vue({
+  el: '#app',
+  data: {
+    username: '',
+    password: '',
+    message: '',
+    messageIsHidden: true,
+    messageIsError: false,
+    messageIsSuccess: false
+  },
+  methods: {
+    login() {
+        var data = new URLSearchParams();
+        data.append('username', app.username);
+        data.append('password', app.password);
+        axios.post('login', data
+        )
+        .then(function (response) {
+            console.log(response);
+            app.message = 'Logged in.';
+            app.messageIsSuccess = true;
+            app.messageIsHidden = false;
+            app.messageIsError = !app.messageIsSuccess;
+        })
+        .catch(function (error) {
+            app.message = error.response.data;
+            app.messageIsHidden = false;
+            app.messageIsError = true;
+            app.messageIsSuccess = !app.messageIsError;
+            console.log(error.response.data);     
+        })
     }
-    $('button').click(function() {
-        console.log('clicked');
-        const credentials = { 'username': $('#username').val(), 'password': $('#password').val() }
-        $.post('/login', credentials)
-              .done(function(data, status, xhr) {
-                  window.localStorage.setItem('token', data[0].access_token);
-                  $.ajaxSetup({ 
-                      headers:{ "Authorization": "Bearer " + window.localStorage.token }
-                  });
-                  console.log('Data ' + data[0].access_token + status + xhr);
-                  updateStatus('Logging in...', 'success');
-                  $.get(data[1].redirect, function(data, status) {
-                      console.log("GET Data " + data + "Status " + status);
-                      if (status == 'success') {
-                          $("#mainView").replaceWith(data);
-                      } else {
-                          updateStatus('Could not start live view.', 'danger');
-                      }
-                  });      
-              })
-              .fail(function(xhr, status, error) {
-                  console.log('Fail ' + xhr.responseText + status + error);
-                  updateStatus(xhr.responseText, 'danger');
-              });
-        });
-
-    function updateStatus(statusMsg, alertType) {
-        $('#status').html('<div class="alert alert-' + alertType + '">' + statusMsg + '</div>');
-    };
-
-});
+  },
+  delimiters: ['[[',']]']
+})
 
 
-// $(document).ready(function() {
-//         console.log("ready");
-//         if (window.localStorage.token) {
-//             window.localStorage.removeItem('token');
-//         }
-//         $("button").click(function() {
-//             console.log("clicked");
-//             const credentials = { "username": $("#username").val(), "password": $("#password").val() }
-//             $.post("/login", credentials, function(data, status) {
-//                     console.log("Data: " + data[0].access_token + " Redirect " + data[1].redirect + "\nStatus: " + status);
-//                     window.localStorage.setItem('token', data[0].access_token);
-//                     $.ajaxSetup({ 
-//                         headers:{ "Authorization": "Bearer " + window.localStorage.token }
-//                     });
-//                     if (data[1].redirect) {
-//                       $.get(data[1].redirect, function(data, status) {
-//                           console.log("GET Data " + data + "Status " + status);
-//                           if (status == 'success') {
-//                               $("#mainView").replaceWith(data);
-//                               console.log("replaced");
-//                           } else {
-//                               $("#mainView").replaceWith("<div> Could not start live view. </div>");
-//                           }
-//                       });
-//                     } else {
-//                         console.log("Incorrect credentials.");
-//                         $("#status").html("<div class='alert alert-warning'>Incorrect username or password.</div>");
-//                     }
-                    
-//             });
-//         });
-// });
+$('.ui.form')
+  .form({
+    fields: {
+      username : 'empty',
+      password : ['minLength[6]', 'empty']
+    }
+  })
+;
--- wispysvr.py
+++ wispysvr.py
@@ -1,12 +1,61 @@
 from flask import Flask, request, abort, render_template, jsonify, redirect, url_for
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, jwt_refresh_token_required, create_refresh_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, jwt_refresh_token_required, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_jwt_cookies
+from flask_socketio import SocketIO, Namespace, emit, join_room, leave_room
+from flask_cors import CORS, cross_origin
 from mongoengine import errors
 import db
+import functools
+import logging
 
 app = Flask(__name__)
 app.config.from_envvar('APP_CONFIG')
 jwt = JWTManager(app)
+CORS(app, origins='http://localhost:8081', supports_credentials=True)
+socketio = SocketIO(app, engineio_logger=True)
 db.connect()
+
+
+def authentication_required(f):
+    """
+    Custom decorator function to check if the user is authenticated before allowing
+    socketio connections.
+    """
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        print('authentication_required called ', args, kwargs)
+    return wrapped
+
+
+@socketio.on('connect', namespace='/')
+# @authentication_required
+def socket_connection():
+    print('Clients connected', socketio.server.manager.rooms['/'])
+    print('Socket io client cookies ', request.cookies)
+    emit('wispysvr', 'You are connected')
+
+
+@socketio.on('disconnect', namespace='/')
+def socket_disconnection():
+    print('Client disconnected', socketio.server.manager.rooms['/'])
+
+
+@socketio.on('authorization')
+def on_authorization(json):
+    # print(json['data'])
+    join_room(json['data']['user'])
+    print('Clients connected room', socketio.server.manager.rooms['/'])
+    print('Socket io auth test req headers: ', request.cookies)
+
+
+@socketio.on('log out')
+def on_log_out(data):
+    print(data['data'])
+    leave_room(data['data'])
+
+
+@socketio.on('wispysvr-frontend', namespace='/')
+def socket_message(message):
+    print('wispy-frontend: ', message)
 
 
 @app.route('/')
@@ -16,29 +65,33 @@ def login_view():
 
 
 @app.route('/login', methods=['POST'])
-def login_process():
+def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         unauthorized = ('Bad username or password.', 401)
         if db.check_user_exists(username):
+            ''' User exists '''
             if db.check_user_password(username, password):
+                ''' Password matches hash '''
                 user = db.get_user(username)
-                tokens = {'access_token': create_access_token(identity=user.username),
-                          'refresh_token': create_refresh_token(identity=user.username)}
-                redirect = {'redirect': url_for('live_view')}
-                print(tokens)
-                return jsonify(tokens, redirect), 200
+                authorized = jsonify({ 'authorized': True })
+                access_token = create_access_token(identity=user.username)
+                refresh_token = create_refresh_token(identity=user.username)
+                set_access_cookies(authorized, access_token)
+                set_refresh_cookies(authorized, refresh_token)
+                return authorized
             else:
                 return unauthorized
         else:
             return unauthorized
 
-    
-@app.route('/liveview')
-@jwt_required
-def live_view():
-    return render_template('probes.html', user=get_jwt_identity())
+
+@app.route('/logout')
+def logout():
+    response = jsonify({ 'authorized': False })
+    unset_jwt_cookies(response)
+    return (response, 200)
 
  
 @app.route('/wispy', methods=['POST'])
@@ -51,7 +104,7 @@ def probe_receiver():
 
 @app.route('/probes')
 @jwt_required
-def probes(): 
+def probes():
     return (db.get_probes_by_time(), 200)
 
 
@@ -64,5 +117,6 @@ def refresh():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True) # pragma: no cover
+    # app.run(host='0.0.0.0', debug=True) # pragma: no cover
+    socketio.run(app, host='0.0.0.0') # pragma: no cover
     
